f7d0045d7772ad70c478ce7b8245307f615e3481
Sanadevel <102668204+Sanadevel@users.noreply.github.com>
2022-11-14 20:20:54 +0900

    1차 애니메이션 + 디자인 완료

--- FrontEnd/LogInPage.js
+++ FrontEnd/LogInPage.js
@@ -9,15 +9,57 @@ const formChangeButton = document.getElementById('form-change-button')
 var count = 0;
 button.addEventListener("click", () => {
     if (count % 2 == 0){
-        count++;
-        changeForm.style.animationPlayState = "running";
-        loginForm.style.animationPlayState = "running";
-        signupForm.style.animationPlayState = "running";
-        setTimeout(() => changeSub.textContent = "Welcome back!", 650);
-        setTimeout(() => changeContent.innerHTML = "To keep connected with us please<br/> login with your profile info", 650);
-        setTimeout(() => formChangeButton.textContent = "Login", 650);
+        running();
+        textChange();
+        
+        signupForm.style.visibility = "visible";
+        
+        setTimeout(() => {
+            loginForm.style.visibility = "hidden";
+        }, 1000);
+    }
+        
+    
+    else{
+        running();
+        textChange();
+        
+        loginForm.style.visibility = "visible";
+        setTimeout(() => {
+            signupForm.style.visibility = "hidden"; 
+        }, 1000);
+        
+    }
+    count++;
+})
+
+function running(){
+    changeForm.style.animationPlayState = "running";
+    loginForm.style.animationPlayState = "running";
+    signupForm.style.animationPlayState = "running";
+    setTimeout(() => {changeForm.style.animationPlayState = "paused"}, 2000);
+    setTimeout(() => {
+        loginForm.style.animationPlayState = "paused";
+        signupForm.style.animationPlayState = "paused";
+    }, 1000);
+}
+function textChange(){
+    let currentText = null;
+    if(count % 2 == 0){
+        currentText = changedText;
     }
     else{
-
+        currentText = originText;
     }
-})
+    setTimeout(() => changeSub.textContent = currentText[0], 650);
+    setTimeout(() => changeContent.innerHTML = currentText[1], 650);
+    setTimeout(() => formChangeButton.textContent = currentText[2], 650);
+}
+
+const changedText = ["Welcome back!", "To keep connected with us please<br/> login with your profile info", "Login"];
+const originText = ['Welcome to GFO', 'Enter your personal details and<br>start journey with us', 'Sign up'];
+
+set(()=>{
+    loginForm.style.visibility = "visible";
+    signupForm.style.visibility = "hidden";
+});
